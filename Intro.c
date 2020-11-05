94c4af7cabf7f7ed09094e85a3b2c8330e8b0b39
insaanehacker <64041206+insaanehacker@users.noreply.github.com>
2020-11-05 18:31:50 +0530

    Create Intro.c

---
+++ Intro.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ //now lets draw a little shape
+int main()
+{
+    printf("   /|\n");
+    printf("  / |\n");
+    printf(" /  |\n");        // now lets build and run
+    printf("/___|\n");
+    return 0;
+}
