52204f84c2482361397fad06d4e50eca2ed105a2
insaanehacker <64041206+insaanehacker@users.noreply.github.com>
2020-11-05 18:43:33 +0530

    Create Comments.c

---
+++ Comments.c
@@ -0,0 +1,14 @@
+//comments are used to ignore a part of code or text in our program
+//we use comments to tell, what that particular line is going to do
+// we can use " // " ....to comment a line
+//If we want to comment a block of code of data...we use " /* */ "
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*int main()
+{
+    printf("Hello world!\n");  //Here whole thing is commented, means it gets ignored by c while running the program
+    return 0;
+}
+*/
